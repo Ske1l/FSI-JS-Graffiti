d1c7ab4d868a8626dabafc7a9582701512d4e8b6
Ske1l <hayley6236@gmail.com>
2022-03-29 18:52:23 -0400

    style changes made

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
+let header = document.querySelector(`#page-header`) //selects page-header id in div tag
+header.style.textAlign = "left" //sets the text alignment in that selector
 
+let dogImages = document.querySelectorAll(`.dog-image`) //selects the dog-image class in all img tags
+for (let i = 0; i < dogImages.length; i++) {  //sets function to go through each image for dogs on next line
+    dogImages[i].style.borderRadius = `50px`
+}
 // Select some elements...
